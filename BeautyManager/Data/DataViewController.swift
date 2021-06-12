9bf17fbcd50092a1a742ff5e0f3f7ad28d9e9efe
SurbineHuang <>
2021-06-12 19:08:20 +0800

    update data

--- BeautyManager/Data/DataViewController.swift
+++ BeautyManager/Data/DataViewController.swift
@@ -10,12 +10,14 @@ import Charts
 
 class DataViewController: UIViewController {
     
+    @IBOutlet weak var chartView: PieChartView!
     @IBOutlet weak var typeSegmented: UISegmentedControl!
-    
-    @IBOutlet weak var pieChartView: PieChartView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+         
+        self.chartView.frame = CGRect(x: 20, y: 80, width: self.view.bounds.width, height: 250)
+        self.view.addSubview(chartView)
         
     }
     
